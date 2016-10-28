b2c0f30191bf0db17a420499f604c4e83d884eb4
Fabian Geiselhart <me@f4814n.de>
2016-10-28 20:49:03 +0200

    Lokale Variablen erstellt; Namen geändert
    * Lokale RGB-Variablen erstellt, da die RGB auserhalb nicht verwendet
    * werden
    
    * {Right,Left,Middle}Line zu ...Color umbenannt

--- firmware/includings/spurenerkennung.h
+++ firmware/includings/spurenerkennung.h
@@ -7,17 +7,21 @@
 
 void RefreshColor()
 {
+	int Leftred, Leftgreen, Leftblue;
+	int Rightred, Rightgreen, Rightblue;
+
+
 	(HTCS2readRGB(colorSensorLeft, Leftred, Leftgreen, Leftblue)); //Linker RGB Sensor auslesen
 	(HTCS2readRGB(colorSensorRight, Rightred, Rightgreen, Rightblue)); //Rechter RGB Sensor auslesen
 
-	if(Leftred < 100) LeftLine = 1; //Wenn ROT Links Spur vorhanden
-	else LeftLine = 0; //Ansonsten keine Spur
+	if(Leftred < 100) LeftLine = 1; //Wenn ROT Links schwarz
+	else LeftColor = 0; //Ansonste weiss
 
-	if(Rightred < 100) RightLine = 1; //Wenn ROT Rechts Spur vorhanden
-	else RightLine = 0; //Ansonsten keine Spur
+	if(Rightred < 100) RightLine = 1; //Wenn ROT Rechts schwarz
+	else RightColor = 0; //Ansonsten weiss
 
 
-	if(SensorValue(colorSensorMiddle) <= 50) MiddleLine = 0;
-	else MiddleLine = 0;
+	if(SensorValue(colorSensorMiddle) <= 50) MiddleLine = 0; //Macht keinen Sinn...
+	else MiddleColor = 0;
 
 }
