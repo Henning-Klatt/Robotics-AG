527f15eab89a73b5e8b105a2c106f55b8b53db56
Fabian Geiselhart <me@f4814n.de>
2016-10-28 20:45:16 +0200

    Variablen sind jetzt Lokal
    * RGB-Werte der Sensoren sind nun in Lokalen variablen gespeichert
    * colorSensor{left,middle,right} meines erachtens nicht nötig da durch
    * #pragma erstellt
    
    * {Right,Left,Middle}Line zu {Right,Left,Middle}Color umbenannt, da ich
    * den Namen für besser halte(Später Farben schwarz weiss und grün)

--- firmware/includings/variablen.h
+++ firmware/includings/variablen.h
@@ -1,4 +1,2 @@
-int colorSensorLeft, colorSensorMiddle, colorSensorRight;
-int Leftred, Leftgreen, Leftblue; //RGB Werte Sensor Links
-int Rightred, Rightgreen, Rightblue; //RGB Werte Sensor rechts
-int LeftLine, MiddleLine, RightLine; // 0 oder 1 wenn Spur
+int LeftColor, MiddleColor, RightColor; // 0 für schwarz; 1 für weiss; (später 2 für grün
+
