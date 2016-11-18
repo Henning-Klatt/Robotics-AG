b74b802821a28d1336f82ca4b7670f1620cd8897
Fabian Geiselhart <me@f4814n.de>
2016-11-18 15:23:37 +0100

    Freitags Cleanup 11.18

--- firmware/includings/spurenerkennung.h
+++ firmware/includings/spurenerkennung.h
@@ -4,24 +4,56 @@
 
 #include "drivers/hitechnic-colour-v2.h"
 #include "includings/variablen.h"
+#include "includings/move.h"
+
+int Leftred, Leftgreen, Leftblue; //RGB Werte Sensor Links
+int Rightred, Rightgreen, Rightblue; //RGB Werte Sensor rechts
 
 void RefreshColor()
 {
-	int Leftred, Leftgreen, Leftblue;
-	int Rightred, Rightgreen, Rightblue;
+	HTCS2readRGB(colorSensorLeft, Leftred, Leftgreen, Leftblue); //Linker RGB Sensor auslesen
+	HTCS2readRGB(colorSensorRight, Rightred, Rightgreen, Rightblue); //Rechter RGB Sensor auslesen
 
+	if(Leftred < 100) LeftLine = COLOR_BLACK; //Wenn ROT Links Spur vorhanden
+	else LeftLine = COLOR_WHITE; //Ansonsten keine Spur
 
-	(HTCS2readRGB(colorSensorLeft, Leftred, Leftgreen, Leftblue)); //Linker RGB Sensor auslesen
-	(HTCS2readRGB(colorSensorRight, Rightred, Rightgreen, Rightblue)); //Rechter RGB Sensor auslesen
-
-	if(Leftred < 100) LeftLine = COLOR_BLACK; //Wenn ROT Links schwarz
-	else LeftColor = COLOR_WHITE; //Ansonste weiss
-
-	if(Rightred < 100) RightLine = COLOR_BLACK; //Wenn ROT Rechts schwarz
-	else RightColor = COLOR_WHITE; //Ansonsten weiss
+	if(Rightred < 100) RightLine = COLOR_BLACK; //Wenn ROT Rechts Spur vorhanden
+	else RightLine = COLOR_WHITE; //Ansonsten keine Spur
 
 
 	if(SensorValue(colorSensorMiddle) <= 50) MiddleLine = COLOR_BLACK;
-	else MiddleColor = COLOR_WHITE;
+	else MiddleLine = COLOR_WHITE;
 
+	nxtDisplayBigTextLine(1, "%d", LeftLine);
+	nxtDisplayBigTextLine(3, "%d", MiddleLine);
+	nxtDisplayBigTextLine(5, "%d", RightLine);
+
+
+}
+
+
+void FollowLine(const int speed)
+{
+	//____________By_Markus_Manz_____________//
+	if(MiddleLine == COLOR_BLACK || RightLine == COLOR_WHITE || LeftLine == COLOR_WHITE){
+		move(speed, speed);
+	}
+
+	if(LeftLine == COLOR_BLACK || MiddleLine != COLOR_BLACK){
+		move(0, speed);
+	}
+
+	if(RightLine == COLOR_BLACK || MiddleLine != COLOR_BLACK){
+		move(speed, 0);
+	}
+	
+	
+	//________NICHT MEHR BY Markus_Manz___________//
+	if(RightLine == COLOR_BLACK || MiddleLine == COLOR_BLACK) {
+		move(speed, -10);
+	}
+
+	if(LeftLine == COLOR_BLACK || MiddleLine == COLOR_BLACK) {
+		move(-10, speed);
+	}
 }
