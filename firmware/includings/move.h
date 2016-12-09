c371436dcd0d7f2b0c44cc7840ebb37c5b04c493
Fabian Geiselhart <me@f4814n.de>
2016-12-09 15:31:56 +0100

    Better

--- firmware/includings/move.h
+++ firmware/includings/move.h
@@ -1,10 +1,40 @@
-#ifndef MOVE_H_
-#define MOVE_H_
-
-void move (const int Left, const int Right)
+void move(int motorl,int motorr,int step,int time)
 {
-	motor[motorA] = Left;
-	motor[motorB] = Right;
+	step *= 3;
+	if(step != 0)
+	{
+		if(abs(motorl) > abs(motorr))
+		{
+			nMotorEncoder[motorB] = 0;
+			while(abs(nMotorEncoder[motorB]) <= step)
+			{
+				motor[motorA] = motorr;
+				motor[motorB] = motorl;
+			}
+		}
+		else
+		{
+			nMotorEncoder[motorA] = 0;
+			while(abs(nMotorEncoder[motorA]) <= step)
+			{
+				motor[motorA] = motorr;
+				motor[motorB] = motorl;
+			}
+		}
+		motor[motorA] = 0;
+		motor[motorB] = 0;
+	}
+	else if(time != 0)
+	{
+		motor[motorA] = motorr;
+		motor[motorB] = motorl;
+		wait1Msec(time);
+		motor[motorA] = 0;
+		motor[motorB] = 0;
+	}
+	else
+	{
+		motor[motorA] = motorr;
+		motor[motorB] = motorl;
+	}
 }
-
-#endif
