24ae0c9a42d5b6bd095ac0852130a600137fbf37
Henning Klatt <henning.klatt@web.de>
2017-01-27 15:30:25 +0100

    Update

--- firmware/includings/variablen.h
+++ firmware/includings/variablen.h
@@ -1,10 +1,5 @@
-#ifndef VARIABLEN_H_
-#define VARIABLEN_H_
-
 #define COLOR_BLACK 0
 #define COLOR_WHITE 1
 #define COLOR_GREEN 2
 
-int LeftLine, RightLine, MiddleLine;
-
-#endif
+int LeftLine, RightLine, MiddleLine, count;
