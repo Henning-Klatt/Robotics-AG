9aa5aeeeae040e09b66964f6aedf52cb059950d9
Fabian Geiselhart <me@f4814n.de>
2017-05-19 16:52:03 +0200

    Removed uneccessary test files. Moved florian.py to test_florian.py

---
+++ Robot/test_florian.py
@@ -0,0 +1,13 @@
+from Brick import *
+import time
+BrickPiSetup()
+BrickPi.MotorEnable[PORT_A] = 1
+BrickPi.MotorEnable[PORT_B] = 1
+while(True):
+    motorRotateDegree([100,100],[300,300],[PORT_A,PORT_B])
+    motorRotateDegree([100,100],[-300,300],[PORT_A,PORT_B])
+    time.sleep(2)
+    motorRotateDegree([100,100],[-300,-300],[PORT_A,PORT_B])
+    motorRotateDegree([100,100],[300,-300],[PORT_A,PORT_B])
+    time.sleep(2)
+
