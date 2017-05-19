e4f8c0d0d8e53c44649bb73c80de0e244905dc22
Fabian Geiselhart <me@f4814n.de>
2017-05-19 16:55:52 +0200

    Added Thread functionality.
    Also started implementing the sensorbar.

--- Robot/Robot.py
+++ Robot/Robot.py
@@ -14,6 +14,8 @@ class Robot(object):
         self.sample = sample # Delay used by motorRotateDegree
         self.speedl = 0
         self.speedr = 0
+        self.colors = [0]*8 # Contains color Values
+        self.colorsCalibrate = [0]*8 # Contains treshold
 
         # Setup
         #-------------------------------
@@ -27,15 +29,15 @@ class Robot(object):
         BrickPiUpdateValues()
 
         # Thread start
-        self.motorThread = Thread(target=self.motor_thread, args=())
+        self.motorThread = Thread(target=self._thread, args=())
         self.motorThread.setDaemon(1)
         self.motorThread.start()
 
 
     def ReadChannel(self):
         """ Function to read SPI data from MCP3008 chip """
-        data = []
-        for i in range(7):
+        data = [0]*8
+        for i in range(8):
             adc = spi.xfer2([1, (8+i)<<4, 0])
             data[i] = ((adc[1]&3) << 8) + adc[2]
         return data
@@ -49,8 +51,20 @@ class Robot(object):
             self.speedr = speed
 
 
-    def motor_thread(self):
+    def motorRotateDegree(self, power,deg,port,sample,delay):
+        """ Frontend to Brick.MotorRotateDegree 
+        TODO: Look into threadsafe version of the library and maybe rewrite mRD() to use it"""
+        self.speedl = -1
+        self.speedr = -1
+        motorRotateDegree(power, deg, [self.motorl, self.motorr], self.sample, .01)
+
+    def _thread(self):
+        """ Makes motors run for longer timespans.
+        TODO: Research Threading class (and use it instead of this) """
         while True:
-            BrickPi.MotorSpeed[self.motorl] = self.speedl
-            BrickPi.MotorSpeed[self.motorr] = self.speedr
-            BrickPiUpdateValues()
+            if self.speedl != -1:
+                BrickPi.MotorSpeed[self.motorl] = self.speedl
+            if self.speedr != -1:
+                BrickPi.MotorSpeed[self.motorr] = self.speedr
+
+                BrickPiUpdateValues()
