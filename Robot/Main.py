e208e9e642277f871fa58363b37c1ba9caadbf3f
f4814n <me@f4814n.de>
2018-02-23 23:21:59 +0000

    Improve 90 Degree

--- Robot/Main.py
+++ Robot/Main.py
@@ -13,7 +13,6 @@ class main(object):
             print("start")
             while True:
                 self.followline()
-                print(rob.light)
                 #debug.showValues(rob)
                 sleep(rob.sample)
     
@@ -29,22 +28,46 @@ class main(object):
             rob.close()
             sys.exit(0)
 
+
     def followline(self):
         """ Follow the line """
         rob.sensorbar()
-        rob.motor('lr', rob.baseSpeed)
 
         if (rob.light[3] == BLACK and rob.light[4] == BLACK):
             rob.motor('rl', rob.baseSpeed)
+            return 
         elif (BLACK in rob.light[4:7]):
-            print("r")
-            rob.motor('r', -rob.baseSpeed * 1.5)
-            rob.motor('l', rob.baseSpeed * 1.5)
-            sleep(.1)
+            O = 7; OM = 6; MI = 5; I = 4;
+            forw = 'l'; back = 'r'
+            every = [O, OM, MI, I]
         elif (BLACK in rob.light[0:3]):
-            print("l")
-            rob.motor('l', -rob.baseSpeed * 1.5)
-            rob.motor('r', rob.baseSpeed * 1.5)
+            O = 0; OM = 1; MI = 2; I = 3
+            forw = 'r'; back = 'l'
+            every = [O, OM, MI, I]
+        else:
+            rob.motor('lr', rob.baseSpeed)
+            return
+        #elif (BLACK in rob.light[4:7]):
+        #    print("r")
+        #    rob.motor('r', -rob.baseSpeed * 1.5)
+        #    rob.motor('l', rob.baseSpeed * 1.5)
+        #    sleep(.1)
+        #elif (BLACK in rob.light[0:3]):
+        #    print("l")
+        #    rob.motor('l', -rob.baseSpeed * 1.5)
+        #    rob.motor('r', rob.baseSpeed * 1.5)
+        #if all([ rob.light[x] for x in every]) :
+        #    print("90")
+        #    rob.motor(forw, rob.baseSpeed, 90, blocking=False)
+        #    rob.motor(back, -rob.baseSpeed, 180)
+        #    return
+        if O == BLACK:
+            rob.motor(back, -rob.baseSpeed, 50, blocking=False)
+            rob.motor(forw, rob.baseSpeed, 50)
+            print("0")
+            return
+        rob.motor(back, -rob.baseSpeed)
+        rob.motor(forw, rob.baseSpeed)
 
 if __name__ == "__main__":
     m = main()
